4d33703d5de7461ad7ec2cccb2dc59081040e253
qinghe <iwangqinghe@gmail.com>
2018-10-27 12:29:59 +0800

    初始化

---
+++ unix-programming/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  unix-programming
+//
+//  Created by qinghe on 2018/10/27.
+//  Copyright © 2018年 sdlmac8. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
